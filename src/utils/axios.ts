5f34b996095634eca8b2eb0a28fdeadfde2b0e09
Foster13 <muhmdfaza@gmail.com>
2025-08-20 16:28:04 +0700

    feat: improvement page profile and adding favorite

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,17 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
-  headers: {
-    "ngrok-skip-browser-warning": "true",
-  },
-});
-
-const axiosServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "ngrok-skip-browser-warning": "true",
-  },
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "https://bf64d6793881.ngrok-free.app/api/v1",
+    headers: {
+      "ngrok-skip-browser-warning": "true", 
+    }
 });
 
 export { axiosInstance, axiosServer };
